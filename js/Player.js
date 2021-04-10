203e531f5693ccabca4cc8c53237b38ecfb6edf4
vageeshn <alokatulya@gmail.com>
2021-04-10 18:01:03 +0530

    test

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -31,5 +32,17 @@ class Player {
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
     })
+  }
+
+  static getCarsAtEnd(){
+    database.ref('CarsAtEnd').on("value",(data)=>{
+      this.rank = data.val();
+    })
+  }
+  
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      CarsAtEnd: rank
+    });
   }
 }
